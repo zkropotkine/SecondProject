22ea985d9ce3ed0f21f9a6d1be0eab066b998085
zkropotkine <s.daniel.rodriguez@gmail.com>
2014-10-07 23:42:22 -0500

    Initial Commit

---
+++ SecondApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondApp
+//
+//  Created by Daniel Rodriguez on 10/7/14.
+//  Copyright (c) 2014 Daniel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecondApp
+//
+//  Created by Daniel Rodriguez on 10/7/14.
+//  Copyright (c) 2014 Daniel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
